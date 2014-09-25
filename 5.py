1afc27ced40a7634fe89aaa025301148f1d760ee
5225225 <mrhouse991@gmail.com>
2014-09-25 07:13:17 +0100

    DOB Days/Seconds calculator

---
+++ 5.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime, date
+
+try:
+    day, month, year = raw_input("Enter DOB formated day/month/year").split("/")
+except ValueError:
+    print("Invalid date")
+
+try:
+    DOB = datetime(int(year), int(month), int(day))
+except ValueError:
+    print("Invalid date")
+now = datetime.now()
+
+delta = now - DOB
+
+print("You have been alive for {} days and {} seconds!".format(
+    delta.days,
+    delta.seconds))
